aacf58f0c7541d7b98ef87166da51ab13c5e4107
sheljasherin <sheljakudallur@gmail.com>
2024-10-30 14:50:16 +0530

    Added my project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { TannyComponent } from './nwebsite/tanny/tanny.component';
+
+
+export const routes: Routes =[
+  {
+    path:'',
+    component:TannyComponent,
+  },
+]
+
+
---
+++ src/app/nwebsite/nwebsite.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NwebsiteRoutingModule } from './nwebsite-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NwebsiteRoutingModule
+  ]
+})
+export class NwebsiteModule { }
